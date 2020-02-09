a5a974611ada3ffc67657a7c72ee7b397e665b8a
DevUrielMC <urimarcervantes.95@gmail.com>
2020-02-08 21:31:03 -0600

    Create the main class whit thow object, and compair the intern content

---
+++ src/com/javacollections2books/app/UsoLibro.java
@@ -0,0 +1,25 @@
+package com.javacollections2books.app;
+
+import com.javacollections2books.model.Libro;
+
+public class UsoLibro {
+
+	public static void main(String[] args) {
+		
+		//Instanciamos dos objetos de tipo libro y los parametrizamos
+		Libro libro1 = new Libro ("El principito", "Uriel",654651);
+		
+		Libro libro2 = new Libro ("Libro del amor", "Carmen", 654651);
+		
+		//Segmento de código para comparar los dos objetos 
+		if(libro1.equals(libro2))
+		{
+			System.out.println("Los libros son iguales");
+			
+		}else {
+			System.out.println("Los libros no son iguales");
+		}
+		
+	}
+
+}
