17813e9c24ec169a956e766de1db7830b9cfd3e1
DevUrielMC <urimarcervantes.95@gmail.com>
2020-02-08 20:55:36 -0600

    Create the POJO of Libro

--- src/com/javacollections2books/model/Libro.java
+++ src/com/javacollections2books/model/Libro.java
@@ -2,12 +2,12 @@ package com.javacollections2books.model;
 
 public class Libro {
 
-	
-		
+		//Declaración de variables privadas
 		private String titulo;
 		private String autor;
 		private int ISBN;
 		
+		//Constructor parametrizado
 		public Libro(String titulo, String autor, int ISBN) {
 			this.titulo=titulo;
 			this.autor=autor;
@@ -15,6 +15,7 @@ public class Libro {
 			
 		}
 		
+		//Metodo para visualizar los parametros de los objetos libro
 		public String getDatos() {
 			return "El titulo es: " + titulo + " El autor es: "+ autor + " El ISBN ES: " + ISBN;
 					
